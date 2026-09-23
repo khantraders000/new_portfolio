d216c91be6d84e5a218664b69d080f9cd6092abb
Ayan Khan <ayan180013@gmail.com>
2026-09-23 21:52:30 +0530

    sitemap update

--- frontend/src/app/sitemap.js
+++ frontend/src/app/sitemap.js
@@ -1,10 +1,28 @@
 export default function sitemap() {
   return [
     {
-      url: "https://itsayan.in",
+      url: "https://itsayan.in/",
       lastModified: new Date(),
       changeFrequency: "monthly",
       priority: 1,
+    },
+    {
+      url: "https://itsayan.in/about",
+      lastModified: new Date(),
+      changeFrequency: "monthly",
+      priority: 0.8,
+    },
+    {
+      url: "https://itsayan.in/projects",
+      lastModified: new Date(),
+      changeFrequency: "monthly",
+      priority: 0.8,
+    },
+    {
+      url: "https://itsayan.in/contact",
+      lastModified: new Date(),
+      changeFrequency: "monthly",
+      priority: 0.7,
     },
   ];
 }
